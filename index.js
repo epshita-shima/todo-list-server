426344dab09e0c9ae98f8093f48cbb043ed8a693
epshita-shima <epshitashima@gmail.com>
2024-01-17 18:10:23 +0600

    add notification

--- index.js
+++ index.js
@@ -20,24 +20,28 @@ async function run() {
     await client.connect();
     const taskCollection = client.db("todoList").collection("task");
     const userCollection = client.db("todoList").collection("users");
+    const notificationCollection = client.db("todoList").collection("notification");
     app.get("/task", async (req, res) => {
       const query = {};
       const cursor = taskCollection.find(query);
       const data = await cursor.toArray();
+      console.log(data)
       res.send(data);
     });
+
     // app.get('/task/:id', async (req, res) => {
     //     const id = req.params.id;
     //     const query = { _id: new ObjectId(id) };
     //     const completeTask = await taskCollection.findOne(query);
     //     res.send(completeTask);
     // })
+
     app.get("/info", async (req, res) => {
       // const ipAddress =  req.headers['x-forwarded-for'] || req.connection.remoteAddress;
       const ipAddress = "202.84.35.56";
       const currentDate = new Date();
       const formattedDate = currentDate.toISOString();
-  
+
       const response = await axios.get(
         `https://ipinfo.io?token=${IPSTACK_API_KEY}`
       );
@@ -57,18 +61,34 @@ async function run() {
       const result = await taskCollection.insertOne(newTask);
       res.send(result);
     });
-    app.get("/userInfo", async (req, res) => {
+    app.get("/info", async (req, res) => {
       const query = {};
       const cursor = userCollection.find(query);
       const data = await cursor.toArray();
+      console.log(data);
       res.send(data);
     });
-    app.post("/", async (req, res) => {
+    app.post("/info", async (req, res) => {
       const newTask = req.body;
       console.log("addning new info", newTask);
       const result = await userCollection.insertOne(newTask);
       res.send(result);
     });
+    app.get("/noticationData", async (req, res) => {
+      const query = {};
+      const cursor = notificationCollection.find(query);
+      const data = await cursor.toArray();
+      console.log(data);
+      res.send(data);
+    });
+
+    app.post("/notification", async (req, res) => {
+      const newTask = req.body;
+      console.log("addning new notification", newTask);
+      const result = await notificationCollection.insertOne(newTask);
+      res.send(result);
+    });
+
     app.put("/task/:id", async (req, res) => {
       const updateTask = req.body.data;
       console.log(updateTask, "pin");
@@ -82,10 +102,14 @@ async function run() {
           time: updateTask.time,
           deadlineDate: updateTask.deadlineDate,
           status: updateTask.status,
+          priviousStatus:updateTask.priviousStatus,
+          markNotification:updateTask.markNotification,
           startTime: updateTask.startTime,
           remarks: updateTask.remarks,
           pinTask: updateTask.pinTask,
           taskPriority: updateTask.taskPriority,
+          taskCompletionDate: updateTask.taskCompletionDate,
+          taskSubmissionDate: updateTask.taskSubmissionDate,
         },
       };
       const result = await taskCollection.updateOne(
@@ -95,8 +119,9 @@ async function run() {
       );
       res.send(result);
     });
-    app.put("/", async (req, res) => {
+    app.put("/project", async (req, res) => {
       const updateTask = req.body;
+      console.log(updateTask);
       const id = updateTask._id;
       const filter = { _id: new ObjectId(id) };
       const options = { upsert: true };
@@ -106,10 +131,14 @@ async function run() {
           time: updateTask.time,
           deadlineDate: updateTask.deadlineDate,
           status: updateTask.status,
+          priviousStatus:updateTask.priviousStatus,
+          markNotification:updateTask.markNotification,
           startTime: updateTask.startTime,
           remarks: updateTask.remarks,
           pinTask: updateTask.pinTask,
           taskPriority: updateTask.taskPriority,
+          taskCompletionDate: updateTask.taskCompletionDate,
+          taskSubmissionDate: updateTask.taskSubmissionDate,
         },
       };
       const result = await taskCollection.updateOne(
@@ -161,6 +190,7 @@ async function run() {
       const result = await taskCollection.deleteOne(query);
       res.send(result);
     });
+
   } finally {
   }
 }
